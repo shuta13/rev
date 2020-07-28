618dd00d075ed3c1a7d3c2001af063d3a7f826e6
shuta13 <hollystarsun@gmail.com>
2020-07-28 12:54:07 +0900

    feat: audio controller

--- components/partials/Controller.tsx
+++ components/partials/Controller.tsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { AppButton } from "../common/AppButton"
 
-export const Controller: React.FC<{ propOnClick: () => void }> = ({ propOnClick }) => {
+export const Controller: React.FC<{ propOnClick?: () => void; text?: string }> = ({ propOnClick, text }) => {
+  const [isPlaying, setIsPlaying] = useState(false)
+  const [playButtonText, setPlayButtonText] = useState("Play")
+  useEffect(() => {
+    isPlaying ? setPlayButtonText("Pause") : setPlayButtonText("Play")
+  }, [isPlaying])
   return (
     <div className="Controller">
       <AppButton
-        propOnClick={propOnClick}
-        text="Play"
+        propOnClick={
+          () => {
+            propOnClick()
+            setIsPlaying(!isPlaying)
+          }
+        }
+        text={text !== undefined ? text : playButtonText}
       />
     </div>
   )
--- components/partials/Player.tsx
+++ components/partials/Player.tsx
@@ -15,7 +15,6 @@ import {
   AudioAnalyser,
   LuminanceFormat,
 } from "three";
-import { AppButton } from "../common/AppButton";
 import { Controller } from "./Controller";
 
 const vert = require("../../assets/shaders/index.vert");
@@ -32,10 +31,22 @@ type GetSpectrumByFftParams = {
   analyser: AudioAnalyser;
   uniforms: any;
 };
+type AudioConfig = {
+  fftSize: number;
+  listener: AudioListener | null;
+  audio: Audio | null;
+  file: string | null;
+}
 
 let isNeedsStopAnimate = false;
 let RAFId = 0;
 let fftRAFId = 0;
+const audioConfig: AudioConfig = {
+  fftSize: 512,
+  listener: null,
+  audio: null,
+  file: null
+}
 
 const handleResize = (renderer: WebGLRenderer) => {
   isNeedsStopAnimate = true;
@@ -46,35 +57,33 @@ const handleResize = (renderer: WebGLRenderer) => {
 const getSpectrumByFft = ({ analyser, uniforms }: GetSpectrumByFftParams) => {
   analyser.getFrequencyData();
   uniforms.audioTexture.value.needsUpdate = true;
-
-  console.log(uniforms.audioTexture);
-
   fftRAFId = requestAnimationFrame(() => {
     getSpectrumByFft({ analyser, uniforms });
   });
 };
 
 const handleOnClick = (uniforms) => {
-  const fftSize = 512;
-  const listener = new AudioListener();
-  const audio = new Audio(listener);
-  const file = "/audio/set-me-free.mp3";
+  if (audioConfig.audio.isPlaying) {
+    audioConfig.audio.pause()
+  } else {
+    const audioLoader = new AudioLoader();
+    audioLoader.load(audioConfig.file, (buffer) => {
+      audioConfig.audio.setBuffer(buffer);
+      audioConfig.audio.play();
+      const analyser = new AudioAnalyser(audioConfig.audio, audioConfig.fftSize);
+      uniforms.audioTexture.value = new DataTexture(
+        analyser.data,
+        audioConfig.fftSize / 2,
+        1,
+        LuminanceFormat
+      );
+      getSpectrumByFft({ analyser, uniforms });
+    });
+  }
 
-  const audioLoader = new AudioLoader();
-  audioLoader.load(file, (buffer) => {
-    audio.setBuffer(buffer);
-    audio.play();
-  });
-
-  const analyser = new AudioAnalyser(audio, fftSize);
-  uniforms.audioTexture.value = new DataTexture(
-    analyser.data,
-    fftSize / 2,
-    1,
-    LuminanceFormat
-  );
-
-  getSpectrumByFft({ analyser, uniforms });
+  if (fftRAFId !== 0) {
+    cancelAnimationFrame(fftRAFId)
+  }
 };
 
 const animate = ({
@@ -140,6 +149,13 @@ const GLSL: React.FC = () => {
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.render(scene, camera);
     window.addEventListener("resize", () => handleResize(renderer));
+
+    // audio init
+    audioConfig.fftSize = 512;
+    audioConfig.listener = new AudioListener();
+    audioConfig.audio = new Audio(audioConfig.listener);
+    audioConfig.file = "/audio/set-me-free.mp3"
+
     animate({ scene, camera, renderer, uniforms, clock });
   };
   useEffect(() => {
